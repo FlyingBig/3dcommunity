c1ae33f71537607b1df4b9b9caa92552cd43f3d6
FlyingBig <1070419969@qq.com>
2019-10-18 09:39:15 +0800

    rem

--- utils/util.js
+++ utils/util.js
@@ -1,9 +1,22 @@
 /**
  * @name:
- * @description: 项目中的一些配置
+ * @description: 项目中的一些配置/方法
  * @author: chenqy
  * @modifiedBy: chenqy
  * @modifiedTime: 2019/10/16
  */
 const basePath = process.env.NODE_ENV === 'development' ? './' : '/3dCommunity/dist';
 export const basePth = basePath;
+
+export const computRem = (doc, win) => {
+    let docEl = doc.documentElement,
+      resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      let clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 32 * (clientWidth / 3200) + 'px';
+    };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+}
